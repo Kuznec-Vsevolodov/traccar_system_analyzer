87d680973257d2b9475fff84110576d712d4a847
Kuznec-Vsevolodov <andrei.pechirevitch@yandex.ru>
2022-04-06 12:09:28 +0600

    map-test-commit5

--- app/Http/Controllers/RequestsController.php
+++ app/Http/Controllers/RequestsController.php
@@ -16,7 +16,8 @@ class RequestsController extends Controller
         $lesson = Lessons::where('id', $lesson_id)->first();
         echo $lesson->lesson_start;
         echo $lesson->lesson_end;
-        $positions = TcPositions::where('deviceid', $lesson->device_id)->whereBetween('devicetime', [$lesson->lesson_start, $lesson->lesson_end])->pluck('longitude', 'latitude');
+        echo TcPositions::all();
+        // $positions = TcPositions::where('deviceid', $lesson->device_id)->whereBetween('devicetime', [$lesson->lesson_start, $lesson->lesson_end])->pluck('longitude', 'latitude');
         return $positions;
         // return view('requests');
     }
